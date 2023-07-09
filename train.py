333d30e2de20fb8b4d96a5b4fc3984cb2ded847f
pacochow <paco.ckchow@gmail.com>
2023-07-09 17:36:32 +0100

    Added HD target

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ params = {
 
 'num_steps': [64, 97],                  # Range of iterations during training
 'pool_size': 128,       
-'batch_size': 5,
+'batch_size': 4,
 'lr': 2e-3,
 'milestones': [3000, 5000, 7000],       # Milestones for learning rate scheduler
 'gamma': 0.3,                           # Gamma factor for learning rate scheduler
