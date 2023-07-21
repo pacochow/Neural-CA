c30323a48529052d7d320526321684b6104dc342
pacochow <paco.ckchow@gmail.com>
2023-07-21 21:49:34 +0100

    Changed target

--- retrain.py
+++ retrain.py
@@ -41,7 +41,7 @@ params = ObjectView(params)
 
 # Get target image
 
-img = np.load("./media/snake.npy")
+img = np.load("./media/snake_yellow.npy")
 target_img = pad_image(img, params.grid_size)
 
 model_name = "experimental"
