dda771cd89292a19a4603828966e83ba506b8121
pacochow <paco.ckchow@gmail.com>
2023-08-22 14:48:33 +0100

    Train model

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import torch
 
 torch.cuda.empty_cache()
 
-device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 params = {
     
 #   Model params
