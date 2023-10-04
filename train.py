e6a76a98cef6269c34a51e2a208993fcbf9849e5
pacochow <paco.ckchow@gmail.com>
2023-10-04 19:38:55 +0100

    Train model

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ params = {
 'model_channels': 16, 
 'env_channels': 2,    
 'n_layers': 2,                          # Number of hidden layers   
-'hidden_units': [50, 380],         # Number of units in hidden layers
+'hidden_units': [70, 280],         # Number of units in hidden layers
 'fire_rate': 0.5,
         
 # Training params
