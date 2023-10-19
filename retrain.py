3e5b8bbab3af9bbf550d349225009a5043e99f4a
pacochow <paco.ckchow@gmail.com>
2023-10-19 10:09:18 +0100

    Retrain model

--- retrain.py
+++ retrain.py
@@ -45,7 +45,7 @@ np.random.seed(0)
 
 # Get target image
 
-img = np.load("./media/ladybug.npy")
+img = np.load("./media/gecko_small_legs.npy")
 target_img = pad_image(img, params.grid_size)
 
 model_name = "experimental"
